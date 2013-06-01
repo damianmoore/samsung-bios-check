bc2cb11d06da4db34437016763ed13e8f9bcf1a0
Damian Moore <damian@epixstudios.co.uk>
2013-06-01 12:45:55 +0100

    BIOS checker script, readme and license file

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-samsung-bios-check
+Samsung BIOS Check
 ==================
 
-BIOS update checker for Samsung laptops running Linux
+*BIOS update checker for Samsung laptops running Linux*
+
+If you have a Samsung laptop and you run Linux, you may know that updating the BIOS/firmware is a hassle. The website doesn't show the firmware version updates available for download so you have to boot Windows and run their application just to check. There are also so many hardware version numbers for each product line, depending on keyboard layout and other hardware differences, that it can be hard to know what yours is.
+
+Running this script will allow you to see easily from Linux (and possibly other UNIX OSs) whether there is an update of your BIOS. You'll still need to boot into Windows to perform an actual upgrade but this should make the process a bit less painful.
+
+The script gets your current BIOS version number via [dmidecode](http://www.nongnu.org/dmidecode/) so for this reason it requires root permissions. It then downloads the same XML from Samsung that their own Windows updater uses and compares version numbers. You can read my original [blog post](https://epixstudios.co.uk/blog/2012/12/01/samsung-laptop-firmware-update-check-from-linux/) for info about understanding their updater software.
+
+To perform the upgrade once you boot Windows you'll need to go to the support section of the Samsung site, find your product, find 'Manuals & Downloads', then under 'Firmware' there'll be an item called 'Update Software (Firmware) (ver.1.0.0.X)'. Click the blue 'EXE' button/link to download 'BIOSUpdate.exe'.
+
+This has been tested on a couple of NP900X3C (Series 9) machines but should work on others unless Samsung change their numbering system or update software. Contributions welcome.
---
+++ samsung_bios_check.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from re import findall
+from subprocess import Popen, PIPE
+from urllib import urlopen
+
+
+def run_command(cmd):
+    p = Popen(cmd.split(' '), stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    return p.communicate()[0].strip()
+
+
+def dependencies_met(dependencies=[]):
+    for dependency in dependencies:
+        if not len(run_command('which %s' % dependency)):
+            return False
+    return True
+
+
+def main():
+    dependencies = ['dmidecode', ]
+    if not dependencies_met(dependencies):
+        print 'You need to have the following installed on your system: %s' % ', '.join(dependencies)
+        exit(1)
+
+    bios_str = run_command('dmidecode -s bios-version')
+    if not bios_str:
+        print 'Couldn\'t get BIOS version from dmidecode, try running as root (with sudo)'
+        exit(1)
+    if not findall(r'P[0-9]{2}[A-z]{3}', bios_str):
+        print 'Sorry, I only understand BIOS versions in the format P11AAA. Yours is %s' % bios_str
+        exit(1)
+    bios_version = int(bios_str[1:3])
+    bios_model = bios_str[3:]
+    print 'BIOS version installed: %d (%s)' % (bios_version, bios_str)
+
+    url = 'http://sbuservice.samsungmobile.com/BUWebServiceProc.asmx/GetContents?platformID=%s&PartNumber=AAAA' % bios_model
+    response = urlopen(url).read()
+
+    try:
+        web_str = findall(r'<Version>([A-Z0-9]+)</Version>', response)[0]
+        web_version = int(web_str[1:3])
+        print 'BIOS version available: %d (%s)' % (web_version, web_str)
+
+        if web_version > bios_version:
+            print '\nBIOS UPDATE AVAILABLE!'
+        else:
+            print '\nYour BIOS is up to date'
+    except IndexError:
+        print 'Sorry, got a bad response from the Samsung website:\n\n%s' % response
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
