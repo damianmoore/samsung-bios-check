f2aaf498bd67c962e72a6fc56732a419b735f4c6
Damian Moore <damian@epixstudios.co.uk>
2013-06-01 20:05:38 +0100

    Loosened version number matching based on feedback from user

--- samsung_bios_check.py
+++ samsung_bios_check.py
@@ -27,11 +27,11 @@ def main():
     if not bios_str:
         print 'Couldn\'t get BIOS version from dmidecode, try running as root (with sudo)'
         exit(1)
-    if not findall(r'P[0-9]{2}[A-z]{3}', bios_str):
-        print 'Sorry, I only understand BIOS versions in the format P11AAA. Yours is %s' % bios_str
+    if not findall(r'[0-9]{2}[A-z]{2,3}', bios_str):
+        print 'Sorry, I only understand BIOS versions in the format [0-9]{2}[A-z]{2,3}. Yours is %s' % bios_str
         exit(1)
-    bios_version = int(bios_str[1:3])
-    bios_model = bios_str[3:]
+    bios_version = int(findall(r'[0-9]{2}', bios_str)[0])
+    bios_model = findall(r'[A-Z]{2,3}', bios_str)[0]
     print 'BIOS version installed: %d (%s)' % (bios_version, bios_str)
 
     url = 'http://sbuservice.samsungmobile.com/BUWebServiceProc.asmx/GetContents?platformID=%s&PartNumber=AAAA' % bios_model
@@ -39,7 +39,7 @@ def main():
 
     try:
         web_str = findall(r'<Version>([A-Z0-9]+)</Version>', response)[0]
-        web_version = int(web_str[1:3])
+        web_version = int(findall(r'[0-9]{2}', web_str)[0])
         print 'BIOS version available: %d (%s)' % (web_version, web_str)
 
         if web_version > bios_version:
